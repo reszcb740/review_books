64df04972dcfed7b79af6331aac94b66e2d3c692
reszcb740 <reszcb740@Gmail.com>
2022-05-13 02:50:01 +0000

    Add public/books

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -1,13 +1,28 @@
 class Public::BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  def create
+   @book = Book.new(book.params)
+   @book.customer_id = current_customer.id
+   @book.save
+   redirect_to public_books_path
   end
 
   def index
+    @books = Book.all
+    @customer = current_customer
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
