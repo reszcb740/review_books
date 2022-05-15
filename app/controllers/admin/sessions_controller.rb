e8fd4b524e526a8339efa3c93a2e619fae62a8d0
reszcb740 <reszcb740@Gmail.com>
2022-05-15 08:40:11 +0000

    Add customers

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,4 +24,14 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resouce)
+    admin_customers_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
