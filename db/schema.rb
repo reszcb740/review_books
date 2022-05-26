691a9d9d30808e25322138458a07cabf0126b985
reszcb740 <reszcb740@Gmail.com>
2022-05-26 08:00:15 +0000

    Finish public/genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_110917) do
+ActiveRecord::Schema.define(version: 2022_05_26_075031) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2022_05_17_110917) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
     t.boolean "is_deleted", default: false
-    t.string "address"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
