60c1f69ea87a4fcfb8cbbd324d61c822c5c10876
reszcb740 <reszcb740@Gmail.com>
2022-06-14 11:36:08 +0000

    Add layouts

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,8 +15,6 @@ class Book < ApplicationRecord
     greater_than_or_equal_to: 0
   }, presence: true
 
-
-
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
